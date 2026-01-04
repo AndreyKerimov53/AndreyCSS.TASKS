d44bc138c1f98341a2207af2b0861d48341e7a2f
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-04 16:45:01 +0300

    tc_4_3

--- gifts2025/routes/index.js
+++ gifts2025/routes/index.js
@@ -7,15 +7,30 @@ router.get('/', function(req, res, next) {
 });
 /* Страница Precious Peach*/
 router.get('/Precious.Peach', function(req, res, next) {
-    res.send("<h1>Страница Precious.Peach</h1>")
+    
+    res.render('gift', { 
+        title: "Precious Peach", // Можно передать данные, специфичные для этого маршрута
+        picture: "images/peach.webp",
+        desc: "Самый дорогой персик в вашей жизни, жаль нельзя съесть."
+    });
 });
-/* Страница Plush Pepe */
+/* Страница Plush Pepe (теперь использует index.ejs) */
 router.get('/Plush.Pepe', function(req, res, next) {
-    res.send("<h1>Страница Plush Pepe</h1>")
+    
+    res.render('gift', { 
+        title: "Plush Pepe", // Можно передать данные, специфичные для этого маршрута
+        picture: "images/plush pepe.jfif",
+        desc: "Самый ценный и очень живой! Этот подарок создан для тех, кто ценит деньги, движение и нескончаемый заряд позитива."
+    });
 });
 /* Страница Durov's Cap */
 router.get('/Durovs.Cap', function(req, res, next) {
-    res.send("<h1>Страница Durovs Cap</h1>")
+    
+    res.render('gift', { 
+        title: "Durov's Cap", // Можно передать данные, специфичные для этого маршрута
+        picture: "images/durov.caps.png",
+        desc: "Кепка с которой вам точно будет не жарко, ведь имея эту кепку вы всегда с вентилятором."
+    });
 });
 
 module.exports = router;
