bd091807d766329a9220decb209ae3f361ea59ad
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-04 18:55:43 +0300

    tc_5_2

--- gifts2025/routes/index.js
+++ gifts2025/routes/index.js
@@ -5,29 +5,38 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* Страница Precious Peach*/
+
+/* Общая страница подарков */
+router.get('/gift', function(req, res, next) {
+  res.render('gift', { 
+    title: 'Подарки',
+    picture: 'images/gift.jpg',
+    desc: 'Выберите подарок из нашей коллекции'
+  });
+});
+
+/* Страница Precious Peach */
 router.get('/Precious.Peach', function(req, res, next) {
-    
     res.render('gift', { 
-        title: "Precious Peach", // Можно передать данные, специфичные для этого маршрута
+        title: "Precious Peach",
         picture: "images/peach.webp",
         desc: "Самый дорогой персик в вашей жизни, жаль нельзя съесть."
     });
 });
-/* Страница Plush Pepe (теперь использует index.ejs) */
+
+/* Страница Plush Pepe */
 router.get('/Plush.Pepe', function(req, res, next) {
-    
     res.render('gift', { 
-        title: "Plush Pepe", // Можно передать данные, специфичные для этого маршрута
+        title: "Plush Pepe",
         picture: "images/plush pepe.jfif",
         desc: "Самый ценный и очень живой! Этот подарок создан для тех, кто ценит деньги, движение и нескончаемый заряд позитива."
     });
 });
+
 /* Страница Durov's Cap */
 router.get('/Durovs.Cap', function(req, res, next) {
-    
     res.render('gift', { 
-        title: "Durov's Cap", // Можно передать данные, специфичные для этого маршрута
+        title: "Durov's Cap",
         picture: "images/durov.caps.png",
         desc: "Кепка с которой вам точно будет не жарко, ведь имея эту кепку вы всегда с вентилятором."
     });
