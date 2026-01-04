b504e38b4e51eb2acbc8b6ddd994f0fff8876840
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-04 21:48:56 +0300

    tc_6_1

---
+++ gifts2025/createDB.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'test2025';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+
+    // Insert test documents
+    const insertResult = await collection.insertMany([
+        { a: 1 }, 
+        { a: 2 }, 
+        { a: 3 }
+    ]);
+    console.log('Inserted documents =>', insertResult);
+
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
