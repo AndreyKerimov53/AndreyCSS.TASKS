4503b7850340d4497b0d97387f953b8acc33b079
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-05 19:02:36 +0300

    tc_9_1

--- gifts2025/app.js
+++ gifts2025/app.js
@@ -3,17 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-// ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 8.1 ============
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tc2025');
+mongoose.connect('mongodb://localhost/tc2025'); // Ваша база tc2025!
+
+// ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 9.1 ============
+var session = require("express-session");
 // ==================================================
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 8.2 ============
 var giftsRouter = require('./routes/gifts');
-// ==================================================
 
 var app = express();
 
@@ -28,28 +27,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 9.1 ============
+// Настройка сессии (добавить ПЕРЕД роутерами)
+app.use(session({
+    secret: "TelegramGifts", // Секретный ключ для подписи сессии
+    cookie: { maxAge: 60 * 1000 }, // Время жизни сессии: 1 минута
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+}));
+// ==================================================
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 8.2 ============
 app.use('/gifts', giftsRouter);
-// ==================================================
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', { 
-    title: 'Telegram Gifts'  
-  });
+    res.status(err.status || 500);
+    res.render('error', { 
+        title: 'Telegram Gifts'  
+    });
 });
 
 module.exports = app;
