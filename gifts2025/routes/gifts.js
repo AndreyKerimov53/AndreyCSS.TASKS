1daa6b9c60b461c03a090ff4f7fb2329c64f915b
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-05 17:53:28 +0300

    tc_8_3

--- gifts2025/routes/gifts.js
+++ gifts2025/routes/gifts.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+// ============ ДОБАВЛЯЕМ ИМПОРТ МОДЕЛИ ============
+var Gift = require('../models/gift').Gift;
+// =================================================
+
 /* GET gifts listing. */
 router.get('/', function(req, res, next) {
     res.send('Новый маршрутизатор, для маршрутов, начинающихся с gifts');
 });
 
 /* Страница подарков с параметром */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    try {
+        // ============ ЗАПРОС К БАЗЕ ДАННЫХ ============
+        var gifts = await Gift.find({nick: req.params.nick});
+        console.log(gifts); // Для отладки
+        
+        if (!gifts.length) {
+            return next(new Error("Нет такого подарка в коллекции"));
+        }
+        
+        var gift = gifts[0];
+        res.render('gift', {
+            title: gift.title,
+            picture: gift.avatar,
+            desc: gift.desc
+        });
+        // ================================================
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
