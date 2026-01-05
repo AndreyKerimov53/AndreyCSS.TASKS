8a9b696f5547895020ffdb849ecf40b0dbbd41b3
AndreyKerimov53 <andreykerimov@gmail.com>
2026-01-06 01:03:47 +0300

    tc_10_1

--- gifts2025/routes/index.js
+++ gifts2025/routes/index.js
@@ -26,4 +26,11 @@ router.get('/gift', function(req, res, next) {
     });
 });
 
+/* ============ ДОБАВЛЕНО ПО ЗАДАНИЮ 10.1 ============ */
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+    res.render('logreg', { title: 'Вход' });
+});
+/* =================================================== */
+
 module.exports = router;
